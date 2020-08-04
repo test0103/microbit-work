e84c4542343f99b374991e762cd9ed0a348e07dc
llz123 <llz123@outlook.com>
2020-08-04 12:06:13 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+startbit.startbit_Init()
+startbit.setServo(startbit.startbit_servorange.range2, 1, 135, 300)
+basic.pause(300)
 basic.forever(function () {
-	
+    if (startbit.startbit_ultrasonic(startbit.startbit_ultrasonicPort.port2) < 20 && startbit.startbit_ultrasonic(startbit.startbit_ultrasonicPort.port2) < 20) {
+        startbit.setServo(startbit.startbit_servorange.range2, 1, 45, 300)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else {
+        basic.pause(1000)
+        startbit.setServo(startbit.startbit_servorange.range2, 1, 135, 300)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(300)
+    }
 })
